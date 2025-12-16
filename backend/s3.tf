629acd109fb6c27c4589647673b19536f6c83eaf
Sarang2401 <Sarang2401@gmail.com>
2025-12-16 21:42:46 +0530

    remote state infra

---
+++ backend/s3.tf
@@ -0,0 +1,15 @@
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "my-terraform-states-example"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
